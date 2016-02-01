f63800b20d9067b40b9b5dc840c440889b61d279
yunchih <yunchih@csie.ntu.edu.tw>
2016-02-01 15:37:47 +0800

    fix broken fail2ban config: jail.conf

--- manifests/ws/fail2ban.pp
+++ manifests/ws/fail2ban.pp
@@ -8,14 +8,14 @@ class ws::fail2ban {
 		ensure	=> 'present'
 	}
 
-	file { '/etc/fail2ban':
-		ensure	=> directory,
-		recurse	=> remote,
-		owner	=> 'root',
-		group	=> 'root',
-		mode	=> '0644',
-		source	=> 'puppet:///wslab/217-base/etc/fail2ban'
-	}
+#	file { '/etc/fail2ban':
+#	    ensure	=> directory,
+#	    recurse	=> remote,
+#       owner	=> 'root',
+#   	group	=> 'root',
+#		mode	=> '0644',
+#		source	=> 'puppet:///wslab/217-base/etc/fail2ban'
+#   }
 
 	service { "fail2ban.service":
 		ensure	=> 'running'
