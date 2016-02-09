13df46c41a13ec2d8a7acc1991e4f50b36cada49
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-02-09 09:20:11 +0800

    Remove unbuildable packages or those inflicted with conflict during installation

--- manifests/ws/pkg.pp
+++ manifests/ws/pkg.pp
@@ -1,26 +1,40 @@
 class ws::pkg {
 
 	$pkgs = hiera("wspkg::pkgs")
+	$unwanted_pkgs = hiera("wspkg::purgelist", [])
 	$pkgs_map = hiera_hash("wspkg::map") # Find all mappings in common.yaml and $::osfamily.yaml
 	$pkgs_mapped = $pkgs.map |$pkg| { $pkgs_map["$pkg"] }
-	$pkgs_resolved = $pkgs_mapped.filter |$pkg| {  ("$pkg" != undef) and ("$pkg" != "") and (! defined(Package["$pkg"]))  }
+	$pkgs_resolved = $pkgs_mapped.filter |$pkg| {  
+		("${pkg}" != undef) and 
+		("${pkg}" != "") 
+	}
 
 	# split packages within same group and trim duplicate packages
 	$pkgs_all = unique( flatten( $pkgs_resolved.map |$pkg| { split($pkg, ' ') } ) ).filter |$pkg| { "$pkg" != "" }
 
 	$agent = $trusted['certname']
-	notice( "Packages installed to node `${osfamily}::${agent}`:" )
-	$pkgs_all.each |$pkg| { notice( $pkg ) }
-	
-	$unwanted_pkgs = hiera("wspkg::purgelist")
-	#$unwanted_pkgs.each |$pkg| { 
-	#	notice( "Purging $pkg" ) 
-	#	package { $pkg:
-	#		ensure	=> 'absent'
-	#	}
-	#}
 
-	ensure_packages( $pkgs_all )
+	notice( "Packages purged on node `${osfamily}::${agent}` to resolve conflict" )
+	$unwanted_pkgs.each |$pkg| { 
+		notice( "Purging $pkg" ) 
+		package { $pkg:
+			ensure => 'absent',
+			uninstall_options => ['--nodeps', '--nodeps']
+		}
+	}
+
+	notice( "Packages installed to node `${osfamily}::${agent}`:" )
+	$pkgs_all.each |$pkg| { 
+		if (! member($unwanted_pkgs, $pkg) ) and (! defined(Package["${pkg}"]) ) { 
+			notice( $pkg ) 
+			package { $pkg:
+				ensure => 'latest'
+			}
+		}
+	}
+	
+
+	#ensure_packages( $pkgs_all )
 
 	#ensure_resource('package', $pkgs_all, { 'ensure' => 'present' })
 }
