3950e019450c4f2ced89e99a4bede0484348c029
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-02-09 09:24:46 +0800

    add sshd_config

--- manifests/ws/ssh.pp
+++ manifests/ws/ssh.pp
@@ -1,9 +1,18 @@
 class ws::ssh {
-    package { 'openssh':
-        ensure => 'latest'
-    }
-	
+	package { 'openssh':
+		ensure => 'latest'
+	}
+
 	service { 'sshd':
 		ensure	=> 'running'
 	}
+
+	file { '/etc/ssh/sshd_config':
+		notify	=> Service['sshd'],
+		owner	=> 0,
+		group	=> 0,
+		mode	=> '0644',
+		source  => 'puppet:///wslab/217-base/etc/ssh/sshd_config'
+	}
+
 }
