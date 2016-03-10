f664b6a7840328027926baf4cd41823f3fa67825
yunchih <yunchih@csie.ntu.edu.tw>
2016-03-10 19:29:28 +0800

    add extra ssh port 80,443 to linux1

--- manifests/ws/ssh.pp
+++ manifests/ws/ssh.pp
@@ -7,12 +7,21 @@ class ws::ssh {
 		ensure	=> 'running'
 	}
 
-	file { '/etc/ssh/sshd_config':
-		notify	=> Service['sshd'],
-		owner	=> 0,
-		group	=> 0,
-		mode	=> '0644',
-		source  => 'puppet:///wslab/217-base/etc/ssh/sshd_config'
-	}
+	$ws_with_extra_ports = hiera("ws::ssh::open_extra_ports")
+
+    if $trusted['certname'] in $ws_with_extra_ports {
+        $source = "puppet:///wslab/217-base/etc/ssh/sshd_config_extra_ports"
+    }
+    else {
+        $source = "puppet:///wslab/217-base/etc/ssh/sshd_config"
+    }
+
+    file { '/etc/ssh/sshd_config':
+        notify	=> Service['sshd'],
+        owner	=> 0,
+        group	=> 0,
+        mode	=> '0644',
+        source  => $source
+    }
 
 }
