9de076bc44404ab872c60ad611a3c76679e5eeb6
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-07-17 14:50:59 +0800

    Make Matlab license server a Systemd service

--- manifests/ws/matlab.pp
+++ manifests/ws/matlab.pp
@@ -10,4 +10,18 @@ class ws::matlab {
 		ensure	=> 'link',
 		target	=> '/nfs/linux/matlab'
 	}	
+
+	$license_server = hiera("ws::matlab::license_server")
+	if $trusted['certname'] in $license_server  {
+        file { '/etc/systemd/system/matlab-lm.service':
+            owner	=> 0,
+            group	=> 0,
+            mode	=> '0644',
+            source  => "puppet:///wslab/217-base/etc/systemd/system/matlab-lm.service"
+        }
+    }
+
+	service { "matlab-lm":
+		ensure	=> 'running'
+	}
 }
