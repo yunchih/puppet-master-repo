c064eda699b6348faeaaf34b6d2354ec2bdac187
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-11-06 20:27:41 +0800

    Refactor SSH && add banlist

--- manifests/ws/ssh.pp
+++ manifests/ws/ssh.pp
@@ -8,21 +8,24 @@ class ws::ssh {
         enable  => true
 	}
 
-	$ws_with_extra_ports = hiera("ws::ssh::open_extra_ports")
+	$extra_ports_servers = hiera("ws::ssh::extra_ports::servers")
+	$extra_ports_list = hiera("ws::ssh::extra_ports")
 
-    if $trusted['certname'] in $ws_with_extra_ports {
-        $source = "puppet:///$environment/217-base/etc/ssh/sshd_config_extra_ports"
+    $fbase = hiera("master::file_base")
+    $source = "${fbase}/${environment}/217-base/etc/ssh/sshd_config"
+    if $trusted['certname'] in $extra_ports_servers  {
+        $extra_ports = join($extra_ports_list.map |$p| { "Port ${p}" }, "\n")
     }
-    else {
-        $source = "puppet:///$environment/217-base/etc/ssh/sshd_config"
-    }
+
+    $ban_list = join(hiera("ws::ssh::ban_list")," ")
+    $sshd_config = join([file($source), "${extra_ports}", "DenyUsers ${ban_list}\n"], "\n")
 
     file { '/etc/ssh/sshd_config':
         notify	=> Service['sshd'],
         owner	=> 0,
         group	=> 0,
         mode	=> '0644',
-        source  => $source
+        content  => $sshd_config
     }
 
 }
