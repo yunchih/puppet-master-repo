c1a808fc01241d29a2076779d267714f7baa158f
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2017-01-03 19:38:23 +0800

    Add systemd user.conf file

--- manifests/ws/systemd.pp
+++ manifests/ws/systemd.pp
@@ -6,4 +6,11 @@ class ws::systemd {
         mode	=> '0644',
         source  => "puppet:///$environment/217-base/etc/systemd/journald.conf"
 	}
+
+	file { '/etc/systemd/user.conf':
+        owner	=> 0,
+        group	=> 0,
+        mode	=> '0644',
+        source  => "puppet:///$environment/217-base/etc/systemd/user.conf"
+	}
 }
