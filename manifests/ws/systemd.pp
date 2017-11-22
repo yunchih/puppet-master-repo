b89a6e117bb79b691de09f8766614128c43c2f36
Yan-Ting Liou <lyt@csie.ntu.edu.tw>
2017-11-22 23:53:31 +0800

    Bind mount /var/log/journal

--- manifests/ws/systemd.pp
+++ manifests/ws/systemd.pp
@@ -13,4 +13,23 @@ class ws::systemd {
         mode	=> '0644',
         source  => "puppet:///$environment/217-base/etc/systemd/user.conf"
 	}
+
+	file { '/etc/tmpfiles.d/systemd.conf':
+        owner	=> 0,
+        group	=> 0,
+        mode	=> '0644',
+        source  => "puppet:///$environment/217-base/etc/tmpfiles.d/systemd.conf"
+	}
+
+	file { '/etc/systemd/system/var-log-journal.mount':
+        owner	=> 0,
+        group	=> 0,
+        mode	=> '0644',
+        source  => "puppet:///$environment/217-base/etc/systemd/system/var-log-journal.mount"
+	}
+
+	service { 'var-log-journal.mount':
+		ensure	=> 'running',
+        enable  => true
+	}
 }
