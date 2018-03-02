7650ea11cdced69ff0e7565c14cbe2239c4abca5
Kuan-Chun Wang <kcwang@csie.ntu.edu.tw>
2018-03-02 13:28:01 +0800

    Don't run Matlab license server on workstation

--- manifests/ws/matlab.pp
+++ manifests/ws/matlab.pp
@@ -15,26 +15,4 @@ class ws::matlab {
 		ensure	=> 'present'
 	}
 
-	$license_server = hiera("ws::matlab::license_server")
-	if $trusted['certname'] in $license_server  {
-        file { '/etc/systemd/system/matlab-lm.service':
-            owner	=> 0,
-            group	=> 0,
-            mode	=> '0644',
-            source  => "puppet:///$environment/217-base/etc/systemd/system/matlab-lm.service"
-        }
-
-        # matlab-lm.service will be deemed 'failed' by SystemD
-        # afte it completes forking.  If set as "ensure => 'running'",
-        # Puppet will try to kill the lm and restart it.
-        service { "matlab-lm":
-            ensure  => undef,
-            enable	=> 'true'
-        }
-
-        file { ['/usr/tmp', '/var/tmp']:
-            ensure  => directory
-        }
-    }
-
 }
