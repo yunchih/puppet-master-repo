d24adba70247e97ab74d9f44da1a9b5a65a79bd7
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2018-03-03 09:15:29 +0800

    Add systemd/timesyncd NTP servers
    Workstations and homedir server should synchronize
    their time with the same NTP server to prevent file
    time skew.
    
    We use one from the government: http://www.stdtime.gov.tw/

--- manifests/ws/systemd.pp
+++ manifests/ws/systemd.pp
@@ -13,4 +13,11 @@ class ws::systemd {
         mode	=> '0644',
         source  => "puppet:///$environment/217-base/etc/systemd/user.conf"
 	}
+
+	file { '/etc/systemd/timesyncd.conf':
+        owner	=> 0,
+        group	=> 0,
+        mode	=> '0644',
+        source  => "puppet:///$environment/217-base/etc/systemd/timesyncd.conf"
+	}
 }
